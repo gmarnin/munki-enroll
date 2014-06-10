4176ef1c1c96a4ebfde7dbc7da577bc10c4e7245
Marnin <marnin.goldberg@rutgers.edu>
2014-06-10 10:32:43 -0400

    email notifications, serial, mostfrequentuser

--- munki-enroll/enroll.php
+++ munki-enroll/enroll.php
@@ -1,51 +1,76 @@
 <?php
 
-require_once( 'cfpropertylist-1.1.2/CFPropertyList.php' );
+namespace CFPropertyList;
+
+// Updated Marnin 4/2014 | Verison 1.20
+
+# Add Logging
+error_reporting( E_ALL );
+ini_set( 'display_errors', 'on' );
+
+require_once( 'CFPropertyList-master/classes/CFPropertyList/CFPropertyList.php' );
+
 
 // Get the varibles passed by the enroll script
-$identifier = $_GET["identifier"];
-$hostname   = $_GET["hostname"];
+$computername		= $_GET["computername"];
+$serial 			= $_GET["serial"];
+$mostfrequentuser	= $_GET["mostfrequentuser"];
 
-// Split the manifest path up to determine directory structure
-$directories		= explode( "/", $identifier, -1 ); 
-$total				= count( $directories );
-$n					= 0;
-$identifier_path	= "";
-while ( $n < $total )
-    {
-        $identifier_path .= $directories[$n] . '/';
-        $n++;
-    }
+// Mail info
+$from = "munki-enroll@domain.com";
+$to = "user@domain.com";
+$subject = "Munki Enroll Notice for $computername";
 
 // Check if manifest already exists for this machine
-if ( file_exists( '../manifests/' . $identifier_path . '/clients/' . $hostname ) )
+if ( file_exists( '../manifests/' . $computername ) )
     {
-        echo "Computer manifest already exists.";
-    }
-else
+    	// Manifest already exists
+    	$message = "manifest already exists for $computername";
+        (mail($to, $subject, $message, "From: " . $from));
+        echo("Manifest already exists for $computername");
+    } 
+ 
+ else
+ 
     {
-        echo "Computer manifest does not exist. Will create.";
-        
-        if ( !is_dir( '../manifests/' . $identifier_path . 'clients/' ) )
-            {
-                mkdir( '../manifests/' . $identifier_path . 'clients/', 0755, true );
-            }
-        
-        // Create the new manifest plist
-        $plist = new CFPropertyList();
-        $plist->add( $dict = new CFDictionary() );
-        
-        // Add manifest to production catalog by default
-        $dict->add( 'catalogs', $array = new CFArray() );
-        $array->add( new CFString( 'production' ) );
-        
-        // Add parent manifest to included_manifests to achieve waterfall effect
-        $dict->add( 'included_manifests', $array = new CFArray() );
-        $array->add( new CFString( $identifier ) );
-        
-        // Save the newly created plist
-        $plist->saveXML( '../manifests/' . $identifier_path . 'clients/' . $hostname );
-        
+    	// Manifest needs to get created
+    	$message = "Client $mostfrequentuser on $computername with serial $serial created a new manifest in munki";
+        (mail($to, $subject, $message, "From: " . $from));
+    	echo("Client $mostfrequentuser on $computername with serial $serial created a new manifest in munki");
     }
 
+                
+        // Create the new manifest plist
+		$plist = new CFPropertyList();
+		$dict = new CFDictionary();
+		$plist->add( $dict );
+        
+		// Add manifest to production catalog by default
+        $dict->add( 'catalogs', $array = new CFArray() );
+        $array->add( new CFString( 'production' ) );
+       
+       	// Add Computer Name and most frequently logged in user. Can be used to verify correct manifest
+        $dict->add( 'notes', $array = new CFArray() );
+	    $array->add( new CFString( 'Serial Number: ' . $serial . ' | Client NetID: ' . $mostfrequentuser ) );
+		
+	    // This format also works. Computer Name and NetID on seprate lines 
+	    // $dict->add( 'Note: Computer Name for this Mac is ' . $computername, $array = new CFArray() );
+	    // $dict->add( 'Note: Client NetID: ' . $mostfrequentuser, $array = new CFArray() );
+						      
+	    // Add parent manifest to included_manifests to achieve waterfall effect
+        $dict->add( 'included_manifests', $array = new CFArray() );
+        $array->add( new CFString( 'standard_config' ) );
+		
+		$dict->add( 'managed_installs', $array = new CFArray() );
+		$dict->add( 'managed_updates', $array = new CFArray() );
+		$dict->add( 'optional_installs', $array = new CFArray() );
+		$dict->add( 'managed_uninstalls', $array = new CFArray() );
+		
+				
+		// Save the newly created plist
+		$plist->saveXML( '../manifests/' . $computername );
+
+//	}
+  
+  
 ?>
