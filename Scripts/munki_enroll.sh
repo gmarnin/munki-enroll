d725d440391d9b9ba292f3f33eef7e20225549d3
Marnin <marnin.goldberg@rutgers.edu>
2014-06-10 10:37:11 -0400

    Added serial, MostFrequentUser

--- Scripts/munki_enroll.sh
+++ Scripts/munki_enroll.sh
@@ -1,20 +1,24 @@
-#!/bin/sh 
- 
-IDENTIFIER=$( defaults read /Library/Preferences/ManagedInstalls ClientIdentifier ); 
-HOSTNAME=$( scutil --get ComputerName );
+#!/bin/sh
 
-SUBMITURL="https://munki/munki-enroll/enroll.php"
+# Script to set the munki manifest to one based on the hostname. Aka one manifest per Mac.
+# Modified by Marnin | 3/14 Version 1.14
 
-# Application paths
-CURL="/usr/bin/curl"
+ComputerName=`/bin/hostname | cut -f1 -d"."`
+MostFrequentUser=`/usr/sbin/ac -p | /usr/bin/sort -nrk 2 | awk 'NR == 2 { print $1; exit }'`
+# Could also add en0 MAC address to help further id the Mac
 
-$CURL --max-time 5 --silent --get \
-    -d hostname="$HOSTNAME" \
-    -d identifier="$IDENTIFIER" \
-    "$SUBMITURL"
- 	
-IDENTIFIER_PATH=$( echo "$IDENTIFIER" | sed 's/\/[^/]*$//' ); 
- 	
-defaults write /Library/Preferences/ManagedInstalls ClientIdentifier "$IDENTIFIER_PATH/clients/$HOSTNAME"
- 
+# Need to check Serial number when run on a Mac Pro
+SERIAL=`/usr/sbin/ioreg -c IOPlatformExpertDevice | grep IOPlatformSerialNumber | awk '{print $4}' | tr -d '"'`
+
+SUBMITURL="https://domain/munki/munki-enroll/enroll.php"
+
+	/usr/bin/curl -H 'Authorization:Basic keygoeshere' --max-time 5 --silent --get \
+	-d computername="$ComputerName" \
+	-d serial="$SERIAL" \
+	-d mostfrequentuser="$MostFrequentUser" \
+	"$SUBMITURL"
+
+# Write the new manifest name
+# /usr/bin/defaults write /private/var/root/Library/Preferences/ManagedInstalls ClientIdentifier "clients/$ComputerName"
+
 exit 0
