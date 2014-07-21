5bb1b3747ca805722ed2b6a44a72e9b7cf1b3889
Marnin <marnin.goldberg@rutgers.edu>
2014-07-21 09:06:54 -0400

    Updated Comments

--- Scripts/munki_enroll.sh
+++ Scripts/munki_enroll.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-# Script to set the munki manifest to one based on the hostname. Aka one manifest per Mac.
-# Modified by Marnin | 3/14 Version 1.14
+# Script to set the munki manifest to one based on the hostname
+# Modified by Marnin
 
 ComputerName=`/bin/hostname | cut -f1 -d"."`
 MostFrequentUser=`/usr/sbin/ac -p | /usr/bin/sort -nrk 2 | awk 'NR == 2 { print $1; exit }'`
-# Could also add en0 MAC address to help further id the Mac
 
-# Need to check Serial number when run on a Mac Pro
+# Using this method to check the serial number which will also work on the old school Mac Pro
 SERIAL=`/usr/sbin/ioreg -c IOPlatformExpertDevice | grep IOPlatformSerialNumber | awk '{print $4}' | tr -d '"'`
 
 SUBMITURL="https://domain/munki/munki-enroll/enroll.php"
@@ -18,7 +17,7 @@ SUBMITURL="https://domain/munki/munki-enroll/enroll.php"
 	-d mostfrequentuser="$MostFrequentUser" \
 	"$SUBMITURL"
 
-# Write the new manifest name
-# /usr/bin/defaults write /private/var/root/Library/Preferences/ManagedInstalls ClientIdentifier "clients/$ComputerName"
+# Write the new manifest name. We're leaving this blank
+# /usr/bin/defaults write /private/var/root/Library/Preferences/ManagedInstalls ClientIdentifier "$ComputerName"
 
 exit 0
